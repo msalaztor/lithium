dc2ca87d0571398e284b67ed5ac0004edeb0a03d
msalaztor <edukatiba@gmail.com>
2023-02-16 11:49:02 -0400

    crudo

---
+++ crudo.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Edukatiba
+Aqui se muestra el precio **De cierre** Del Barril de Crudo!
+""")
+
+#definir el ticker
+tickerSymbol = 'CL=F'
+#obtener datos
+tickerData = yf.Ticker(tickerSymbol)
+#obtener precios historicos
+tickerDf = tickerData.history(period='1d', start='2019-1-1', end='2023-2-13')
+
+
+st.write("""
+## Precio de cierre
+""")
+st.line_chart(tickerDf.Close)
